77ebd19b9facc57e2cbc5ffdc1c12ce6659dcf90
coachcarteruk-gif <coachcarteruk@gmail.com>
2026-02-26 11:38:52 +0000

    Delete middleware.js

--- middleware.js
+++
@@ -1,8 +0,0 @@
-export default function middleware(req) {
-  // Redirect all traffic to maintenance page
-  return Response.redirect(new URL('/maintenance.html', req.url));
-}
-
-export const config = {
-  matcher: ['/((?!maintenance|api).*)'],
-};
